cc4efd568e14eab468d98d9339d3b6ded42ab4d5
Sarvar Muzaffarov <smuzaffarov@anderson.edu>
2025-09-24 21:21:52 -0400

    Test fail function

--- test_AllFunctions.py
+++ test_AllFunctions.py
@@ -10,6 +10,10 @@ def test_divide(client):
     response = client.get("/divide/10/2")
     assert b"Here is your quotient: 5.0" in response.data
 
+def test_divide_fail(client):
+    response = client.get("/divide/10/2")
+    assert b"Here is your quotient: 10000" in response.data
+
 devision = [(a,b, a/b) for a in range(1, 100) for b in range (1, 100)]
 
 @pytest.mark.parametrize("a,b,expected", devision)
