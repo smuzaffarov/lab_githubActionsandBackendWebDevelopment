1fe1e4b959100acd3dce57eda19f1d195d4aa14f
Sarvar Muzaffarov <smuzaffarov@anderson.edu>
2025-09-24 21:31:00 -0400

    Add multiplication test

--- test_AllFunctions.py
+++ test_AllFunctions.py
@@ -10,11 +10,7 @@ def test_divide(client):
     response = client.get("/divide/10/2")
     assert b"Here is your quotient: 5.0" in response.data
 
-def test_divide_fail(client):
-    response = client.get("/divide/10/2")
-    assert b"Here is your quotient: 10000" in response.data
-
-devision = [(a,b, a/b) for a in range(1, 100) for b in range (1, 100)]
+devision = [(a,b, a/b) for a in range(1, 10) for b in range (1, 10)]
 
 @pytest.mark.parametrize("a,b,expected", devision)
 def testDevisionParametrize(client, a, b, expected):
@@ -29,6 +25,13 @@ def testDevisionByZeroParametrize(client, a, b):
     response = client.get(f"/divide/{a}/{b}")
     assert b"Error: division by zero" in response.data
 
+
+multiplications = [(a,b, a*b) for a in range(0, 10) for b in range (1, 10)]
+
+@pytest.mark.parametrize("a,b,expected", multiplications)
+def numberMultiplications(client, a, b, expected):
+    response = client.get(f"/multiplication/{a}/{b}")
+    assert f"Here is your quotient: {expected}".encode() in response.data
 
 
 
