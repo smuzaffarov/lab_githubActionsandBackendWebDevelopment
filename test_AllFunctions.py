5e99f05fe02134ff82bc917426095230eb10e826
Sarvar Muzaffarov <smuzaffarov@anderson.edu>
2025-09-24 21:02:49 -0400

    Add the function that is responsible for the testing divisions functions and divisions on 0

---
+++ test_AllFunctions.py
@@ -0,0 +1,31 @@
+from DifferentExpressionsNumbers import app
+import pytest
+
+
+@pytest.fixture()
+def client():
+    return app.test_client()
+
+def test_divide(client):
+    response = client.get("/divide/10/2")
+    assert b"Here is your quotient: 5.0" in response.data
+
+devision = [(a,b, a/b) for a in range(1, 100) for b in range (1, 100)]
+
+@pytest.mark.parametrize("a,b,expected", devision)
+def testDevisionParametrize(client, a, b, expected):
+    response = client.get(f"/divide/{a}/{b}")
+    assert f"Here is your quotient: {expected}".encode() in response.data
+
+
+devisionByZero = [(a, 0) for a in range(1, 10)]
+
+@pytest.mark.parametrize("a,b", devisionByZero)
+def testDevisionByZeroParametrize(client, a, b):
+    response = client.get(f"/divide/{a}/{b}")
+    assert b"Error: division by zero" in response.data
+
+
+
+
+
