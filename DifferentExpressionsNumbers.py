09cbd7fc86b1c0c7578c89fc6e0f0f9d069e1414
Sarvar Muzaffarov <smuzaffarov@anderson.edu>
2025-09-24 21:05:16 -0400

    Now adding the Python with the actual route

---
+++ DifferentExpressionsNumbers.py
@@ -0,0 +1,36 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+template = """
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Flask Test</title>
+  </head>
+
+  <body>
+    <h1> Claculator Analog </h1>
+    <h2> For Devision --> /divide/10/2 </h2>
+    <h2> For Devision --> /divide/10/2 </h2>
+    <h2> For Devision --> /divide/10/2 </h2>
+    <h2> For Devision --> /divide/10/2 </h2>
+    {content}
+  </body>
+</html>
+""".strip()
+
+
+@app.route('/')
+def defScreen():
+   return ()
+
+
+@app.route('/divide/<int:number_1>/<int:number_2>')
+def divide(number_1, number_2):
+    if number_2 == 0:
+      return template.replace("{content}", "Error: division by zero")
+    result = number_1 / number_2
+    return template.replace("{content}", f"Here is your quotient: {result}")
+
